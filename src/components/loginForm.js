fa24e047a94f36c0ebef9430e7b98e9c07545baf
Alix <dev.alix13@gmail.com>
2023-09-23 22:02:52 +0330

    Revert "Merge pull request #1 from alix1383/deepsource-transform-e2c34f64"
    This reverts commit 5abc0108444e0cd8ee66301043677ad31903d40b, reversing
    changes made to 9dfc3262cbb0996ce84a69fd347cdb43120497ac.

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,60 +1,60 @@
 import {
-  setApiKeyToDB,
-  showList,
-  removeSpaceFromString,
-  checkApiKeyIsOky
-} from '../functions/misc'
+    setApiKeyToDB,
+    showList,
+    removeSpaceFromString,
+    checkApiKeyIsOky,
+} from "../functions/misc";
 
 //! Defind Elements
-const form = document.getElementById('loginForm')
+const form = document.getElementById("loginForm");
 
 /**
  * when user submit loggin button this func run =>
- * 1st get input value
+ * 1st get input value 
  * 2nd remove space from value and check api key
  * 3rd if check passed rander token list else show error
  */
-function showLoginComponent () {
-  //! Defind Elements
-  const submibtLoginFormButton = form.querySelector('a')
+function showLoginComponent() {
+    //! Defind Elements
+    const submibtLoginFormButton = form.querySelector("a");
 
-  //! remove hidden attribut form element
-  form.removeAttribute('hidden', '')
+    //! remove hidden attribut form element
+    form.removeAttribute("hidden", "");
 
-  submibtLoginFormButton.addEventListener('click', async (e) => {
-    //! prevent Events
-    e.preventDefault()
+    submibtLoginFormButton.addEventListener("click", async (e) => {
+        //! prevent Events
+        e.preventDefault();
 
-    //! this line get token input text then remove spaces
-    const inputValue = removeSpaceFromString(
-      form.querySelector('input').value
-    )
+        //! this line get token input text then remove spaces
+        const inputValue = removeSpaceFromString(
+            form.querySelector("input").value
+        );
 
-    //! check input not empty
-    if (inputValue && inputValue !== '') {
-      //! Check APi Key is Oky
-      if (checkApiKeyIsOky()) {
-        //! if Api Key is oky. DO =>
-        //! save ApiKey to DB
-        setApiKeyToDB(inputValue)
+        //! check input not empty
+        if (inputValue && inputValue !== "") {
+            //! Check APi Key is Oky
+            if (checkApiKeyIsOky()) {
+                //! if Api Key is oky. DO =>
+                //! save ApiKey to DB
+                setApiKeyToDB(inputValue);
 
-        //! Show Token List
-        showList()
+                //! Show Token List
+                showList();
 
-        //! hide Login Component
-        hideLoginComponent()
-      } else {
-        //! show error message if token have issuse
-        form.querySelector('p').innerHTML =
-                    'Your Token Is Wrong or Banned'
-      }
-    }
-  })
+                //! hide Login Component
+                hideLoginComponent()
+            } else {
+                //! show error message if token have issuse
+                form.querySelector("p").innerHTML =
+                    "Your Token Is Wrong or Banned";
+            }
+        }
+    });
 }
 
-function hideLoginComponent () {
-  //! remove hidden attribute login form
-  form.setAttribute('hidden', '')
+function hideLoginComponent() {
+    //! remove hidden attribute login form
+    form.setAttribute("hidden", "");
 }
 
-export { hideLoginComponent, showLoginComponent }
+export { hideLoginComponent, showLoginComponent };
